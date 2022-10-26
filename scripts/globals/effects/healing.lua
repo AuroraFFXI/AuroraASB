157fa6f039a517156c9931e887a76a11f31efb67
Aurasplash <45613880+Aurasplash@users.noreply.github.com>
2022-10-26 09:39:30 -0600

    resting HP

--- scripts/globals/effects/healing.lua
+++ scripts/globals/effects/healing.lua
@@ -62,10 +62,12 @@ effectObject.onEffectTick = function(target, effect)
         -- curse II also known as "zombie"
         if not(target:hasStatusEffect(xi.effect.DISEASE)) and target:hasStatusEffect(xi.effect.PLAGUE) == false and target:hasStatusEffect(xi.effect.CURSE_II) == false then
             local healHP = 0
-            -- if target:getContinentID() == 1 and target:hasStatusEffect(xi.effect.SIGNET) then
-                -- healHP = 10 + (3 * math.floor(target:getMainLvl() / 10)) + (healtime - 2) * (1 + math.floor(target:getMaxHP() / 300)) + target:getMod(xi.mod.HPHEAL)
-            if target:getMaster() ~= nil then -- Beastmaster's Stay ability
+            if target:getContinentID() == 1 and target:hasStatusEffect(xi.effect.SIGNET) then
+                healHP = 10 + (3 * math.floor(target:getMainLvl() / 10)) + (healtime - 2) * (1 + math.floor(target:getMaxHP() / 300)) + target:getMod(xi.mod.HPHEAL)
+                target:addTP(xi.settings.main.HEALING_TP_CHANGE)
+            elseif target:getMaster() ~= nil then -- Beastmaster's Stay ability
                 healHP = 10 + (3 * math.floor(target:getMainLvl() / 10)) + (healtime - 2) * (2.5 + math.floor(target:getMaxHP() / 100)) + target:getMod(xi.mod.HPHEAL)
+                target:addTP(xi.settings.main.HEALING_TP_CHANGE)
             else
                 target:addTP(xi.settings.main.HEALING_TP_CHANGE)
                 healHP = 10 + (healtime - 2) + target:getMod(xi.mod.HPHEAL)
